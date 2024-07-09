f26cbd4e26e75f3cf2643a4baa79506e79d1f02f
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2024-07-10 07:34:40 +0900

    feat: LottoWinningManager 구현

--- src/__tests__/LottoWinningManager.test.js
+++ src/__tests__/LottoWinningManager.test.js
@@ -1,17 +1,58 @@
 import { describe, test, expect } from 'vitest'
+import Lotto from '../domain/Lotto.js'
+import LottoWinningManager from '../domain/LottoWinningManager'
+import { ERROR_MESSAGES } from '../constants/errorMessages.js'
 
 describe('LottoWinningManager > ', () => {
-  test(
-    '로또 당첨 번호를 추가하면 기존 로또 번호와 일치하는 당첨 번호를 알 수 있다.'
+  test('로또 당첨 번호를 추가하면 기존 로또 번호와 일치하는 당첨 번호를 알 수 있다.', () => {
+    const lotto = Lotto.generate([1, 2, 3, 4, 5, 6])
+
+    expect(
+      new LottoWinningManager()
+        .generateWinningNumbers([1, 3, 7])
+        .getWinningResult(lotto).main
+    ).toEqual([1, 3])
+  })
+
+  test('이미 당첨된 번호로 또 번호를 추가하면 아무 일도 일어나지 않는다.', () => {
+    const lottoWinningManager =
+      new LottoWinningManager().generateWinningNumbers([1, 3, 7])
+
+    expect(() =>
+      lottoWinningManager.generateWinningNumbers([1, 3, 7])
+    ).not.toThrow()
+    expect(lottoWinningManager.getWinningNumbers()).toEqual([1, 3, 7])
+  })
+
+  test.each([{ amount: 1, amount: 4, amount: 10 }])(
+    '당첨 번호를 원하는 개수만큼 자동으로 추가할 수 있다. : $amount개',
+    ({ amount }) => {
+      expect(
+        new LottoWinningManager()
+          .autoGenerateWinningNumbers(amount)
+          .getWinningNumbers().length
+      ).toBe(amount)
+    }
   )
 
-  test('보너스 당첨 번호를 추가할 수 있다.')
+  test('보너스 당첨 번호를 추가할 수 있다.', () => {
+    expect(new LottoWinningManager().setBonusNumber(7).getBonusNumber()).toBe(7)
+  })
 
-  test('이미 당첨된 번호로 또 번호를 추가하면 아무 일도 일어나지 않는다.')
+  test('보너스 당첨 번호를 자동으로 추가할 수 있다.', () => {
+    const lottoWinningManager = new LottoWinningManager()
+    const autoNum = lottoWinningManager.autoSetBonusNumber().getBonusNumber()
 
-  test('이미 당첨된 번호로 보너스 번호를 추가하면 에러가 발생한다.')
+    expect(autoNum).toBeGreaterThanOrEqual(1)
+    expect(autoNum).toBeLessThanOrEqual(45)
+  })
 
-  test('1 ~ 45 사이의 숫자가 아닌 보너스 번호를 추가하면 에러가 발생한다.')
-
-  test('당첨된 로또 개수를 알 수 있다.')
+  test('1 ~ 45 사이의 숫자가 아닌 보너스 번호를 추가하면 에러가 발생한다.', () => {
+    expect(() => new LottoWinningManager().setBonusNumber(46)).toThrow(
+      ERROR_MESSAGES.INVALID_LOTTO_NUMBER
+    )
+    expect(() => new LottoWinningManager().setBonusNumber(0)).toThrow(
+      ERROR_MESSAGES.INVALID_LOTTO_NUMBER
+    )
+  })
 })
--- src/domain/LottoWinningManager.js
+++ src/domain/LottoWinningManager.js
@@ -1,6 +1,77 @@
+import { LOTTO } from '../constants/lottoValues.js'
+import generateUniqueNumbers from '../utils/generateUniqueNumbers.js'
+import LottoValidator from './LottoValidator.js'
+
 class LottoWinningManager {
+  #winningNumbers
+  #bonusNumber
+
   constructor() {
-    this.winningNumbers = []
-    this.bonusNumber = null
+    this.#winningNumbers = new Set()
+    this.#bonusNumber = null
+  }
+
+  getWinningNumbers() {
+    return [...this.#winningNumbers]
+  }
+
+  getBonusNumber() {
+    return this.#bonusNumber
+  }
+
+  getWinningResult(lotto) {
+    const result = { main: [], bonus: null }
+
+    lotto.getNumbers().forEach((number) => {
+      if (this.#winningNumbers.has(number)) {
+        result.main.push(number)
+      }
+
+      if (number === this.#bonusNumber) {
+        result.bonus = number
+      }
+    })
+
+    return result
+  }
+
+  generateWinningNumbers(winningNumbers) {
+    LottoValidator.validateWinningNumbers(winningNumbers)
+    this.#winningNumbers = new Set([...this.#winningNumbers, ...winningNumbers])
+
+    return this
+  }
+
+  autoGenerateWinningNumbers(amount) {
+    const prevAmount = this.#winningNumbers.size
+    const newWinningNumbers = generateUniqueNumbers(
+      amount + prevAmount,
+      LOTTO.MIN_NUM,
+      LOTTO.MAX_NUM,
+      this.#winningNumbers
+    )
+
+    this.#winningNumbers = new Set([
+      ...this.#winningNumbers,
+      ...newWinningNumbers,
+    ])
+
+    return this
+  }
+
+  setBonusNumber(bonusNumber) {
+    LottoValidator.validateNumber(bonusNumber)
+    this.#bonusNumber = bonusNumber
+
+    return this
+  }
+
+  autoSetBonusNumber() {
+    const bonusNumber = generateUniqueNumbers(1, LOTTO.MIN_NUM, LOTTO.MAX_NUM)
+    this.#bonusNumber = bonusNumber[0]
+
+    return this
   }
 }
+
+export default LottoWinningManager
